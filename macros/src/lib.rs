ea79f656f172ca5857971124659437d39e3f0be0
Tim Snyder <timsnyder@sorellalabs.xyz>
2024-08-14 12:05:34 -0400

    macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -9,6 +9,7 @@ use syn::{Data, DeriveInput, Fields};
 use quote::quote;
 
 mod clickhouse;
+mod postgres;
 
 #[allow(unused_extern_crates)]
 extern crate proc_macro;
@@ -36,6 +37,34 @@ extern crate proc_macro;
 /// ```
 pub fn remote_clickhouse_table(input: TokenStream) -> TokenStream {
     clickhouse::remote_table::remote_clickhouse_table(input.into())
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
+}
+
+#[proc_macro]
+/// macro creates a struct that initializes all functionality
+/// for interacting with postgres tables (must have created a `PostgresDBMS`
+/// with the `postgres_dbms!` macro) There are 3 requied and 3 optional inputs
+///
+/// 1 - enum name of the DBMS (ident)
+/// 2 - name of the schema (string)
+/// 3 - name of the table struct (ident)
+/// 4 (optional) - the type that is used when inserting into the table
+/// 5 (optional) - a tuple of 'child tables' (i.e. tables that will be
+/// created/dropped along with the parent table) 
+/// 6 (optional) the path to the directory where the sql table is defined
+/// (relative to the workspace/crate root), if not provided the testing module
+/// is disabled for the table
+///
+/// Examples:
+/// ```
+/// postgres_table!(DMBS, "db", Table, TableInsertType);
+/// postgres_table!(DMBS, "db", Table, TableInsertType, (LocalRelays));
+/// postgres_table!(DMBS, "db", Table, TableInsertType, (LocalRelays), "path/to/table/dir");
+/// postgres_table!(DMBS, "db", Table, "path/to/table/dir");
+/// ```
+pub fn postgres_table(input: TokenStream) -> TokenStream {
+    postgres::table::postgres_table(input.into())
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
 }
